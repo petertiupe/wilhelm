8d7717bb4506eb78c3817bbab2ff6c1811360e33
peter <peter@tiupe.de>
2022-04-19 16:47:31 +0200

    simplehandler.kt kleinere Anpassungen

--- src/jsMain/kotlin/de/tiupe/handlers/simplehandler.kt
+++ src/jsMain/kotlin/de/tiupe/handlers/simplehandler.kt
@@ -1,19 +1,26 @@
 package de.tiupe.handlers
 
 import dev.fritz2.core.RootStore
-import dev.fritz2.core.SimpleHandler
-
 import dev.fritz2.core.render
+import kotlinx.coroutines.flow.flowOf
 
- object StringStore : RootStore<String>("Peters Punkte") {
+object StringStore : RootStore<String>("Peters Punkte") {
 
-    val appendHandler = handle { model->
+    val appendHandler = handle { model ->
         "$model +"
     }
 
-    val removeHandler = handle{ model->
-        if(model.length > "Peters Punkte".length ) {
+    val removeHandlerWithoutInt = handle { model: String ->
+        if (model.length > "Peters Punkte".length) {
             model.dropLast(1)
+        } else {
+            model
+        }
+    }
+
+    val removeHandler = handle<Int> { model: String, numberToRemove: Int ->
+        if (model.length > "Peters Punkte".length) {
+            model.dropLast(numberToRemove)
         } else {
             model
         }
@@ -28,25 +35,27 @@ fun renderSimpleHandler() {
     render("#simplehandler") {
         div {
             button("addButton") {
-                + "Add To String"
+                +"Add To String"
                 inlineStyle("margin: 5px")
                 clicks handledBy StringStore.appendHandler
             }
 
             button("removeButton") {
-                + "Remove from String"
+                +"Remove from String"
                 inlineStyle("margin: 5px")
-                clicks handledBy StringStore.removeHandler
+                flowOf(1) handledBy StringStore.removeHandler
+                clicks handledBy StringStore.removeHandlerWithoutInt
             }
 
             button("initButton") {
-                + "Initialize"
+                +"Initialize"
                 inlineStyle("margin: 5px")
                 clicks handledBy StringStore.initHandler
             }
-        }
-        StringStore.data.render {
-            p{ + it }
+
+            StringStore.data.render {
+                p { +it }
+            }
         }
     }
 }
